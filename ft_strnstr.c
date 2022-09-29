01d778830a453b7746abd6b98093a4817648613f
Jafar Alwaheidi <jalwahei@lab3r4s8.42abudhabi.ae>
2022-09-29 20:08:54 +0400

    i did it

--- ft_strnstr.c
+++ ft_strnstr.c
@@ -6,7 +6,7 @@
 /*   By: jalwahei <jalwahei@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/27 23:33:19 by jalwahei          #+#    #+#             */
-/*   Updated: 2022/09/27 23:54:32 by jalwahei         ###   ########.fr       */
+/*   Updated: 2022/09/29 20:08:17 by jalwahei         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,6 +28,8 @@ char	*ft_strnstr(const char *haystack, const char *needle, size_t len)
 		i = 0;
 	if (needle_len == 0)
 		return ((char *)haystack);
+	if (ft_strlen(needle) > ft_strlen(haystack))
+	return (NULL);
 	while (i <= (int)(len - needle_len))
 	{
 		if ((haystack[0] == needle[0]) && (0 == ft_strncmp
@@ -37,4 +39,14 @@ char	*ft_strnstr(const char *haystack, const char *needle, size_t len)
 						haystack++;
 	}
 	return (NULL);
+}
+int main ()
+{
+			char *big = "abcdef";
+			char *little = "abcdefghijklmnop";
+			size_t	max = strlen(big);
+			char 	*s1 = strnstr(big, little, max);
+			char 	*s2 = ft_strnstr(big, little, max);
+			printf("%s\n", s1);
+			printf("%s", s2);
 }
